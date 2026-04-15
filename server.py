cfa1fd9481abb4394fda530ac1197430c6f1bc7d
rusagusa1 <rusagusa1@gmail.com>
2026-04-15 19:05:44 +0500

    Update server.py

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ from flask import Flask, request, Response, redirect
 app = Flask(__name__)
 
 VPN_CONFIG_URL = (
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS.txt"
+    "https://cdn.jsdelivr.net/gh/igareck/vpn-configs-for-russia@main/BLACK_VLESS_RUS_mobile.txt"
 )
 OBHOD_CONFIG_URL = (
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt"
+    "https://cdn.jsdelivr.net/gh/igareck/vpn-configs-for-russia@main/Vless-Reality-White-Lists-Rus-Mobile.txt"
 )
 RENDER_URL = "https://cbn-vpn-server.onrender.com/"  # ⚠️ замени на свой URL после деплоя
 SECRET_KEY = "cbn_secret_2026"                        # ⚠️ одинаковый в боте и сервере
